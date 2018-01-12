c32f614701a5619e707a5d1fe60f9e26a88a774c
Dennis Patzer <d.patzer@heimrich-hannot.de>
2018-01-12 17:52:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Contao Utils Bundle
+
+This bundle offers various utility functionality for the Contao CMS.
---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Copyright (c) 2018 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace HeimrichHannot\UtilsBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use HeimrichHannot\UtilsBundle\HeimrichHannotContaoUtilsBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(HeimrichHannotContaoUtilsBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
