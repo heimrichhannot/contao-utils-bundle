17fe2f4a0860ed699ce8d391aebe3c829ee40a84
Dennis Patzer <d.patzer@heimrich-hannot.de>
2020-11-05 10:43:43 +0100

    see cl 2.181.4

--- src/Container/ContainerUtil.php
+++ src/Container/ContainerUtil.php
@@ -80,7 +80,7 @@ class ContainerUtil
 
     public function isFrontendCron()
     {
-        return 'contao_frontend_cron' === $this->getCurrentRequest()->get('_route');
+        return $this->getCurrentRequest() && 'contao_frontend_cron' === $this->getCurrentRequest()->get('_route');
     }
 
     public function isInstall()
