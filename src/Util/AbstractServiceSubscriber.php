b39251c162019dec8ff35910515b0d00d4c33bf8
Thomas Körner <t.koerner@heimrich-hannot.de>
2021-10-13 08:55:11 +0200

    add AbstractServiceSubscriber, fix container util service subscriber

---
+++ src/Util/AbstractServiceSubscriber.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Copyright (c) 2021 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace HeimrichHannot\UtilsBundle\Util;
+
+if (interface_exists('Symfony\Contracts\Service\ServiceSubscriberInterface')) {
+    abstract class AbstractServiceSubscriber implements \Symfony\Contracts\Service\ServiceSubscriberInterface
+    {
+    }
+} elseif (interface_exists('Symfony\Component\DependencyInjection\ServiceSubscriberInterface')) {
+    abstract class AbstractServiceSubscriber implements \Symfony\Component\DependencyInjection\ServiceSubscriberInterface
+    {
+    }
+}
