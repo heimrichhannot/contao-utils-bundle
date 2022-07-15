11d0ad51ab9a082982e08ca349133a665ed7f5f6
Thomas Körner <t.koerner@heimrich-hannot.de>
2022-07-15 11:00:39 +0200

    deprecated old method

--- src/User/UserUtil.php
+++ src/User/UserUtil.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2021 Heimrich & Hannot GmbH
+ * Copyright (c) 2022 Heimrich & Hannot GmbH
  *
  * @license LGPL-3.0-or-later
  */
@@ -43,6 +43,8 @@ class UserUtil
 
     /**
      * @return UserModel|UserModel[]|\Contao\Model\Collection|null
+     *
+     * @deprecated use Utils service instead
      */
     public function findActiveByGroups(array $groups, array $options = [])
     {
