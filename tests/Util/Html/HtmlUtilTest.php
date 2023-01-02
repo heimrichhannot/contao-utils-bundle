5d797be4a6494073f2b3c7d4f599329ef0d73267
Thomas Körner <t.koerner@heimrich-hannot.de>
2023-01-02 12:21:23 +0100

    add HtmlUtil::generateDataAttributesString()

--- tests/Util/Html/HtmlUtilTest.php
+++ tests/Util/Html/HtmlUtilTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2022 Heimrich & Hannot GmbH
+ * Copyright (c) 2023 Heimrich & Hannot GmbH
  *
  * @license LGPL-3.0-or-later
  */
@@ -35,6 +35,66 @@ class HtmlUtilTest extends AbstractUtilsTestCase
         $this->assertSame(
             'src="heimrich-hannot.de" type="text/website" async="async"',
             $instance->generateAttributeString(['src' => 'heimrich-hannot.de', 'type' => 'text/website', 'async' => true], ['xhtml' => true])
+        );
+    }
+
+    public function testGenerateDataAttributesString()
+    {
+        $instance = $this->getTestInstance();
+
+        $this->assertSame('data-foo-bar="1"', $instance->generateDataAttributesString(['Foo Bar' => '1']));
+        $this->assertSame('data-foo-bar="1"', $instance->generateDataAttributesString(['Foo-Bar' => '1']));
+        $this->assertSame('data-foo-bar="1"', $instance->generateDataAttributesString(['FooBar' => '1']));
+
+        $this->assertSame('', $instance->generateDataAttributesString(['Foo Bar' => false]));
+        $this->assertSame('', $instance->generateDataAttributesString([]));
+
+        $this->assertSame(
+            'data-Foo Bar="1"',
+            $instance->generateDataAttributesString(['Foo Bar' => '1'], ['normalizeKeys' => false])
+        );
+        $this->assertSame(
+            'data-Foo-Bar="1"',
+            $instance->generateDataAttributesString(['Foo-Bar' => '1'], ['normalizeKeys' => false])
+        );
+        $this->assertSame(
+            'data-FooBar="1"',
+            $instance->generateDataAttributesString(['FooBar' => '1'], ['normalizeKeys' => false])
+        );
+
+        $this->assertSame(
+            'data-foo-bar="1"',
+            $instance->generateDataAttributesString(['data-foo-bar' => '1'])
+        );
+
+        $this->assertSame(
+            'data-foo-bar',
+            $instance->generateDataAttributesString(['data-foo-bar' => true])
+        );
+        $this->assertSame(
+            'data-foo-bar="Blub"',
+            $instance->generateDataAttributesString(['Foo Bar' => ['Blah' => 'Blub']])
+        );
+        $this->assertSame(
+            'data-foo-bar="Blub"',
+            $instance->generateDataAttributesString(['Foo Bar' => ['Blah' => 'Blub']], ['array_handling' => 'unsupported'])
+        );
+        $this->assertSame(
+            'data-foo-bar="{&quot;Blah&quot;:&quot;Blub&quot;}"',
+            $instance->generateDataAttributesString(['Foo Bar' => ['Blah' => 'Blub']], ['array_handling' => 'encode'])
+        );
+
+        $this->assertSame(
+            'data-foo-bar data-animal-type="bird" data-editable data-some-strange-attribute="Totally strange" data-perfectly-prepared="sure" data-class="button attention" data-count="5"',
+            $instance->generateDataAttributesString([
+                'data-foo-bar' => true,
+                'Animal type' => 'bird',
+                'editable' => true,
+                'Some Strange Attribute' => 'Totally strange',
+                'data-perfectly-prepared' => 'sure',
+                'class' => ['button', 'attention'],
+                'Count' => 5,
+            ])
         );
     }
 }
