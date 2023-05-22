e82b9bd117df4ba620890e0b9c0b7903ba096d1f
Thomas Körner <t.koerner@heimrich-hannot.de>
2023-05-22 15:32:52 +0200

    add absolute url option to RoutingUtil

--- src/Util/Routing/RoutingUtil.php
+++ src/Util/Routing/RoutingUtil.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2022 Heimrich & Hannot GmbH
+ * Copyright (c) 2023 Heimrich & Hannot GmbH
  *
  * @license LGPL-3.0-or-later
  */
@@ -10,8 +10,11 @@ namespace HeimrichHannot\UtilsBundle\Util\Routing;
 
 use Contao\CoreBundle\Csrf\ContaoCsrfTokenManager;
 use HeimrichHannot\UtilsBundle\Util\AbstractServiceSubscriber;
+use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
@@ -45,12 +48,23 @@ class RoutingUtil extends AbstractServiceSubscriber
     /**
      * Generate a backend route with token and referer.
      *
+     * Options:
+     * - absoluteUrl (bool): Return absolute url (default: false)
+     *
      * @param array $params Url-Parameters
+     *
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
      *
      * @return string The backend route url
      */
-    public function generateBackendRoute(array $params = [], bool $addToken = true, bool $addReferer = true, string $route = 'contao_backend'): string
+    public function generateBackendRoute(array $params = [], bool $addToken = true, bool $addReferer = true, string $route = 'contao_backend', array $options = []): string
     {
+        $options = array_merge(
+            ['absoluteUrl' => false],
+            $options
+        );
+
         if ($addToken) {
             // >= contao 4.6.8 uses contao.csrf.token_manager service to validate token
             if ($this->container->has(ContaoCsrfTokenManager::class)) {
@@ -64,7 +78,11 @@ class RoutingUtil extends AbstractServiceSubscriber
             $params['ref'] = $request->get('_contao_referer_id');
         }
 
-        return $this->router->generate($route, $params);
+        return $this->router->generate(
+            $route,
+            $params,
+            $options['absoluteUrl'] ? UrlGeneratorInterface::ABSOLUTE_URL : UrlGeneratorInterface::ABSOLUTE_PATH
+        );
     }
 
     /**
