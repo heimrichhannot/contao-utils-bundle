06ea5ac7061fc4a85a718758a5b83a1792045ee2
Thomas Körner <t.koerner@heimrich-hannot.de>
2024-01-09 16:29:54 +0100

    add inserttag finder helper, use helper in article search

--- src/EntityFinder/EntityFinderHelper.php
+++ src/EntityFinder/EntityFinderHelper.php
@@ -20,6 +20,7 @@ class EntityFinderHelper
         private ContaoFramework $framework,
     )
     {
+        $this->databaseUtil = $databaseUtil;
     }
 
     /**
@@ -41,5 +42,64 @@ class EntityFinderHelper
         $values[] = $type;
 
         return $this->framework->getAdapter(ModuleModel::class)->findBy($columns, $values);
+    }
+
+	/**
+     * Find frontend modules by insert inserttags like insert_module oder insert_article.
+     *
+     * @param string $type The module type
+     * @param string $field The tl_module field
+     * @param string $inserttag The inserttag to search for, for example insert_module
+     * @param int $id The element id to search for, for example the module id (as used in {{insert_module::1}}, would be 1 in this case)
+     * @return array The found module ids
+     * @throws \Exception
+     */
+    public function findModulesByInserttag(string $type, string $field, string $inserttag, int $id): array
+    {
+        if (!Validator::isAlias($field)) {
+            throw new \Exception('Invalid field name '.$field.'given.');
+        }
+        if (!Validator::isAlias($inserttag)) {
+            throw new \Exception('Invalid inserttag '.$inserttag.'given.');
+        }
+        $result = Database::getInstance()
+            ->prepare("SELECT id FROM tl_module
+                        WHERE type=?
+                        AND (
+                            $field LIKE '%{{".$inserttag."::".$id."}}%'
+                            OR $field LIKE '%{{".$inserttag."::".$id."::%')")
+            ->execute($type);
+
+        return $result->fetchEach('id');
+    }
+
+
+    /**
+     * Find content elements by insert inserttags like insert_module oder insert_article.
+     *
+     * @param string $type The element type
+     * @param string $field The tl_content field
+     * @param string $inserttag The inserttag to search for, for example insert_module
+     * @param int $id The element id to search for, for example the module id (as used in {{insert_module::1}}, would be 1 in this case)
+     * @return array The found content element ids
+     * @throws \Exception
+     */
+    public function findContentElementByInserttag(string $type, string $field, string $inserttag, int $id): array
+    {
+        if (!Validator::isAlias($field)) {
+            throw new \Exception('Invalid field name '.$field.'given.');
+        }
+        if (!Validator::isAlias($inserttag)) {
+            throw new \Exception('Invalid inserttag '.$inserttag.'given.');
+        }
+        $result = Database::getInstance()
+            ->prepare("SELECT id FROM tl_content
+                        WHERE type=?
+                        AND (
+                            $field LIKE '%{{".$inserttag."::".$id."}}%'
+                            OR $field LIKE '%{{".$inserttag."::".$id."::%')")
+            ->execute($type);
+
+        return $result->fetchEach('id');
     }
 }
