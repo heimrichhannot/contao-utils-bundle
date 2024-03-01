a32b19074303f811f6b3a819ea30ea96ea9f90a4
Thomas <koertho@users.noreply.github.com>
2024-03-01 10:38:01 +0100

    Add date added field (#74)
    * add date added field
    
    * add test
    
    * fixed type exception
    
    * fixed tests
    
    * increase test coverage, some code adjustments
    
    * increase coverage
    
    * fixed issue with static variables

--- src/Dca/AuthorField.php
+++ src/Dca/AuthorField.php
@@ -2,30 +2,36 @@
 
 namespace HeimrichHannot\UtilsBundle\Dca;
 
-class AuthorField
+class AuthorField extends AbstractDcaField
 {
     public const TYPE_USER = 'user';
     public const TYPE_MEMBER = 'member';
 
-    /** @var array  */
     protected static $tables = [];
 
     /**
-     * Register a dca to have an author field and update logic added.
+     * @return array<AuthorFieldConfiguration>
      */
-    public static function register(string $table): AuthorFieldOptions
+    public static function getRegistrations(): array
     {
-        $config = new AuthorFieldOptions($table);
-
-        static::$tables[$table] = $config;
-
-        return $config;
+        return parent::getRegistrations();
     }
 
     /**
-     * @return array<AuthorFieldOptions>
+     * @param string $table
+     * @return AuthorFieldConfiguration
      */
-    public static function getRegistrations(): array
+    protected static function createOptionObject(string $table): DcaFieldConfiguration
+    {
+        return new AuthorFieldConfiguration($table);
+    }
+
+    protected static function storeConfig(DcaFieldConfiguration $config): void
+    {
+        static::$tables[$config->getTable()] = $config;
+    }
+
+    protected static function loadConfig(): array
     {
         return static::$tables;
     }
---
+++ tests/EventListener/DcaField/DateAddedFieldListenerTest.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace HeimrichHannot\UtilsBundle\Tests\EventListener\DcaField;
+
+use Contao\DataContainer;
+use Contao\Model;
+use HeimrichHannot\UtilsBundle\Dca\DateAddedField;
+use HeimrichHannot\UtilsBundle\Dca\DcaFieldConfiguration;
+use HeimrichHannot\UtilsBundle\EventListener\DcaField\DateAddedFieldListener;
+use PHPUnit\Framework\TestCase;
+
+class DateAddedFieldListenerTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     */
+    public function testOnLoadDataContainer()
+    {
+        $listener = new DateAddedFieldListener();
+        $table = 'test_table';
+
+        // Mock the global array
+        $GLOBALS['TL_DCA'][$table] = [
+            'config' => [],
+            'fields' => [],
+        ];
+
+        $listener->onLoadDataContainer($table);
+
+        $this->assertArrayNotHasKey('onload_callback', $GLOBALS['TL_DCA'][$table]['config']);
+        $this->assertArrayNotHasKey('oncopy_callback', $GLOBALS['TL_DCA'][$table]['config']);
+        $this->assertArrayNotHasKey('dateAdded', $GLOBALS['TL_DCA'][$table]['fields']);
+
+        DateAddedField::register($table);
+
+        $listener->onLoadDataContainer($table);
+
+        $this->assertArrayHasKey('onload_callback', $GLOBALS['TL_DCA'][$table]['config']);
+        $this->assertArrayHasKey('oncopy_callback', $GLOBALS['TL_DCA'][$table]['config']);
+        $this->assertArrayHasKey('dateAdded', $GLOBALS['TL_DCA'][$table]['fields']);
+    }
+
+    public function testOnLoadCallback()
+    {
+        // Without DataContainer
+
+        $listener = $this->getMockBuilder(DateAddedFieldListener::class)
+            ->onlyMethods(['getModelInstance'])
+            ->getMock();
+
+        $listener->expects($this->never())->method('getModelInstance');
+
+        $listener->onLoadCallback();
+
+        // With DataContainer, but no model instance
+
+        $listener = $this->getMockBuilder(DateAddedFieldListener::class)
+            ->onlyMethods(['getModelInstance'])
+            ->getMock();
+
+        $dc = $this->getMockBuilder(DataContainer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $dc->id = 1;
+        $dc->method('__get')->willReturnCallback(function ($name) {
+            switch ($name) {
+                case 'table':
+                    return 'test_table';
+                case 'id':
+                    return 1;
+            }
+            return null;
+        });
+
+        $listener->method('getModelInstance')->willReturn(null);
+
+        $listener->onLoadCallback($dc);
+
+        // Complete run
+
+        $listener = $this->getMockBuilder(DateAddedFieldListener::class)
+            ->onlyMethods(['getModelInstance'])
+            ->getMock();
+
+        $model = $this->getMockBuilder(Model::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['save'])
+            ->getMock();
+
+        $model->dateAdded = 0;
+
+        $listener->method('getModelInstance')->willReturn($model);
+
+        $listener->onLoadCallback($dc);
+
+        $this->assertGreaterThan(0, $model->dateAdded);
+    }
+
+    public function testOnCopyCallback()
+    {
+        $listener = $this->getMockBuilder(DateAddedFieldListener::class)
+            ->onlyMethods(['getModelInstance'])
+            ->getMock();
+
+        $dc = $this->getMockBuilder(DataContainer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $dc->method('__get')->willReturnCallback(function ($name) {
+            switch ($name) {
+                case 'table':
+                    return 'test_table';
+            }
+            return null;
+        });
+
+        $listener->expects($this->never())->method('getModelInstance');
+
+        $listener->onCopyCallback(1, $dc);
+
+        // Run without model
+
+        $dc = $this->getMockBuilder(DataContainer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $dc->method('__get')->willReturnCallback(function ($name) {
+            switch ($name) {
+                case 'table':
+                    return 'test_table';
+                case 'id':
+                    return 1;
+            }
+        });
+
+        $listener = $this->getMockBuilder(DateAddedFieldListener::class)
+            ->onlyMethods(['getModelInstance'])
+            ->getMock();
+
+        $listener->method('getModelInstance')->willReturn(null);
+
+        $listener->onCopyCallback(1, $dc);
+
+        // Complete run
+
+        $listener = $this->getMockBuilder(DateAddedFieldListener::class)
+            ->onlyMethods(['getModelInstance'])
+            ->getMock();
+
+        $dc = $this->getMockBuilder(DataContainer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $dc->method('__get')->willReturnCallback(function ($name) {
+            switch ($name) {
+                case 'table':
+                    return 'test_table';
+                case 'id':
+                    return 1;
+            }
+        });
+
+        $model = $this->getMockBuilder(Model::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['save'])
+            ->getMock();
+
+        $model->dateAdded = 0;
+
+        $listener->method('getModelInstance')->willReturn($model);
+
+        $listener->onCopyCallback(1, $dc);
+
+        $this->assertGreaterThan(0, $model->dateAdded);
+    }
+}
