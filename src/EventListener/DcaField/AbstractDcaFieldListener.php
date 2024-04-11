1b091e634502c5598b7ca0eec5780bf1d41b5611
Thomas <koertho@users.noreply.github.com>
2024-04-11 15:30:05 +0200

    Extend dca field options (#83)
    * enhance dca field config
    
    * fix test
    
    * increase test coverage

--- src/EventListener/DcaField/AbstractDcaFieldListener.php
+++ src/EventListener/DcaField/AbstractDcaFieldListener.php
@@ -4,6 +4,7 @@ namespace HeimrichHannot\UtilsBundle\EventListener\DcaField;
 
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\Model;
+use HeimrichHannot\UtilsBundle\Dca\DcaFieldConfiguration;
 use Psr\Container\ContainerInterface;
 use Symfony\Contracts\Service\ServiceSubscriberInterface;
 
@@ -22,6 +23,29 @@ abstract class AbstractDcaFieldListener implements ServiceSubscriberInterface
         $framework = $this->container->get('contao.framework');
         $modelClass = $framework->getAdapter(Model::class)->getClassFromTable($table);
         return $framework->getAdapter($modelClass)->findByPk($id);
+    }
+
+    protected function applyDefaultFieldAdjustments(array &$field, DcaFieldConfiguration $configuration)
+    {
+        if ($configuration->isFilter()) {
+            $field['filter'] = true;
+        }
+
+        if ($configuration->isSearch()) {
+            $field['search'] = true;
+        }
+
+        if ($configuration->isExclude()) {
+            $field['exclude'] = true;
+        }
+
+        if ($configuration->isSorting()) {
+            $field['sorting'] = true;
+        }
+
+        if ($configuration->getFlag() !== null) {
+            $field['flag'] = $configuration->getFlag();
+        }
     }
 
     public static function getSubscribedServices(): array
