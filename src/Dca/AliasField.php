d1e8042582ef376ed72ef76b62cefc67a968a63c
Thomas Körner <t.koerner@heimrich-hannot.de>
2025-05-20 16:51:10 +0200

    add alias field

---
+++ src/Dca/AliasField.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace HeimrichHannot\UtilsBundle\Dca;
+
+use HeimrichHannot\UtilsBundle\EventListener\DcaField\AliasDcaFieldListener;
+
+class AliasField extends AbstractDcaField
+{
+    private static $tables = [];
+
+    protected static function storeConfig(DcaFieldConfiguration $config): void
+    {
+        static::$tables[$config->getTable()] = $config;
+    }
+
+    protected static function loadConfig(): array
+    {
+        return static::$tables;
+    }
+
+    protected static function createOptionObject(string $table): DcaFieldConfiguration
+    {
+        return new AliasFieldConfiguration($table);
+    }
+
+    public static function getField(): array
+    {
+        return [
+            'label' => &$GLOBALS['TL_LANG']['MSC']['alias'],
+            'exclude' => true,
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['rgxp' => 'alias', 'unique' => true, 'maxlength' => 128, 'tl_class' => 'w50'],
+            'save_callback' => [],
+            'sql' => "varchar(255) BINARY NOT NULL default ''",
+        ];
+    }
+}
---
+++ src/EventListener/DcaField/AliasDcaFieldListener.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace HeimrichHannot\UtilsBundle\EventListener\DcaField;
+
+use Contao\CoreBundle\ServiceAnnotation\Hook;
+use Contao\CoreBundle\Slug\Slug;
+use Contao\Database;
+use Contao\DataContainer;
+use HeimrichHannot\UtilsBundle\Dca\AliasField;
+use HeimrichHannot\UtilsBundle\Dca\AliasFieldConfiguration;
+
+class AliasDcaFieldListener extends AbstractDcaFieldListener
+{
+    /**
+     * @Hook("loadDataContainer")
+     */
+    public function onLoadDataContainer(string $table): void
+    {
+        if (!isset(AliasField::getRegistrations()[$table])) {
+            return;
+        }
+
+        /** @var AliasFieldConfiguration $registration */
+        $registration = AliasField::getRegistrations()[$table];
+
+        $field = AliasField::getField();
+        if (is_array($registration->aliasExistCallback)) {
+            $field['save_callback'][] = $registration->aliasExistCallback;
+        }
+
+        $this->applyDefaultFieldAdjustments($field, $registration);
+
+        $GLOBALS['TL_DCA'][$table]['fields'][$registration->fieldName] = $field;
+    }
+
+    public function onFieldsAliasSaveCallback(mixed $value, DataContainer $dc): mixed
+    {
+        $aliasExists = static function (string $alias) use ($dc): bool {
+            return Database::getInstance()
+                    ->prepare("SELECT $dc->table FROM tl_article WHERE alias=? AND id!=?")
+                    ->execute($alias, $dc->id)
+                    ->numRows > 0;
+        };
+
+        // Generate an alias if there is none
+        if (!$value) {
+            $value = $this->container->get(Slug::class)->generate(
+                (string)$dc->activeRecord->title,
+                (int)$dc->activeRecord->pid,
+                $aliasExists
+            );
+        } elseif (preg_match('/^[1-9]\d*$/', $value)) {
+            throw new \Exception(sprintf($GLOBALS['TL_LANG']['ERR']['aliasNumeric'], $value));
+        } elseif ($aliasExists($value)) {
+            throw new \Exception(sprintf($GLOBALS['TL_LANG']['ERR']['aliasExists'], $value));
+        }
+
+        return $value;
+    }
+
+    public static function getSubscribedServices(): array
+    {
+        return array_merge(
+            [
+                'contao.slug' => Slug::class,
+            ],
+            parent::getSubscribedServices()
+        );
+    }#
+
+
+}
