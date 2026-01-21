949e90903301c377d517bb929186a1adaf17b763
Thomas Körner <t.koerner@heimrich-hannot.de>
2026-01-21 14:15:17 +0100

    allow customize alias source field [ci skip]

--- src/Dca/AliasFieldConfiguration.php
+++ src/Dca/AliasFieldConfiguration.php
@@ -6,24 +6,55 @@ use HeimrichHannot\UtilsBundle\EventListener\DcaField\AliasDcaFieldListener;
 
 class AliasFieldConfiguration extends DcaFieldConfiguration
 {
+    /**
+     * @internal
+     * @deprecated
+     */
     public ?array $aliasExistCallback = [AliasDcaFieldListener::class, 'onFieldsAliasSaveCallback'];
 
+    /**
+     * @internal
+     */
     public string $fieldName = 'alias';
+
+    /**
+     * @internal
+     */
+    public string $titleField = 'title';
+
+    /**
+     * @internal
+     */
+    public ?array $generateAliasCallback = [AliasDcaFieldListener::class, 'onFieldsAliasSaveCallback'];
+
+    /**
+     * Override the default alias exist function. Provide as [Class, 'method'].
+     *
+     * @param array<string, string> $aliasExistCallback
+     * @deprecated Deprecated since version 3.10. Use setGenerateAliasCallback instead.
+     */
+    public function setAliasExistCallback(?array $aliasExistCallback): AliasFieldConfiguration
+    {
+        $this->generateAliasCallback = $aliasExistCallback;
+        return $this;
+    }
 
     /**
      * Override the default alias exist function. Provide as [Class, 'method'].
      *
      * @param array<string, string> $aliasExistCallback
      */
-    public function setAliasExistCallback(?array $aliasExistCallback): AliasFieldConfiguration
-    {
-        $this->aliasExistCallback = $aliasExistCallback;
-        return $this;
-    }
+    public function setGenerateAliasCallback(?array $aliasExistCallback): AliasFieldConfiguration {}
 
     public function setFieldName(string $fieldName): AliasFieldConfiguration
     {
         $this->fieldName = $fieldName;
+        return $this;
+    }
+
+    public function setTitleField(string $titleField): AliasFieldConfiguration
+    {
+        $this->titleField = $titleField;
         return $this;
     }
 }
